64ad0c7345c69f978aebbdc347e85cd574596b96
Karatechop <boris.rugel@gmail.com>
2015-07-09 02:36:34 +0200

    reorganized scripts

--- plot4.R
+++ plot4.R
@@ -17,7 +17,7 @@
 # locating the rows that contain data dated 2007-02-01 and 2007-02-02. This was
 # achieved as follows:
 # 	data<- read.table("./household_power_consumption.txt",
-#			header = TRUE	,
+#			header = TRUE,
 #			sep = ";", 
 #			col.names = c( "Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
 #			na.strings = "?", 
